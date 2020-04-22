1de10e0ac44a0cf9ca96f9adffa6fac6e8ebe351
Seb <sebastien.kremer@gmail.com>
2020-04-22 10:32:18 +0200

    Added new keys in Fn layer for minus and equal

--- keyboards/xd75/keymaps/seb/keymap.c
+++ keyboards/xd75/keymaps/seb/keymap.c
@@ -24,27 +24,27 @@ enum  {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_ortho_5x15(    
-	 	KC_ESC     , KC_1    , KC_2    , KC_3    , KC_4     , KC_5    , KC_MINS , KC_GRV  , KC_EQL  , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_BSPC , 
-	 	KC_TAB     , KC_Q    , KC_W    , KC_E    , KC_R     , KC_T    , KC_LBRC , KC_BSLS , KC_RBRC , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_QUOT , 
-		TO(_NUMPAD), KC_A    , KC_S    , KC_D    , KC_F     , KC_G    , KC_HOME , KC_DEL  , KC_PGUP , KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_ENT  , 
-		KC_LSFT    , KC_Z    , KC_X    , KC_C    , KC_V     , KC_B    , KC_END  , KC_UP   , KC_PGDN , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT , 
-		KC_LCTL    , KC_LGUI , KC_LALT , MO(_FN) , KC_SPC   , KC_SPC  , KC_LEFT , KC_DOWN , KC_RGHT , KC_SPC  , KC_SPC  , MO(_FN) , KC_RALT , KC_RGUI , KC_RCTL
+	 	KC_ESC     , KC_1    , KC_2      , KC_3      , KC_4      , KC_5    , KC_MINS   , KC_GRV  , KC_EQL  , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_BSPC , 
+	 	KC_TAB     , KC_Q    , KC_W      , KC_E      , KC_R      , KC_T    , KC_LBRC   , KC_BSLS , KC_RBRC , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_QUOT , 
+		TO(_NUMPAD), KC_A    , KC_S      , KC_D      , KC_F      , KC_G    , KC_HOME   , KC_DEL  , KC_PGUP , KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_ENT  , 
+		KC_LSFT    , KC_Z    , KC_X      , KC_C      , KC_V      , KC_B    , KC_END    , KC_UP   , KC_PGDN , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT , 
+		KC_LCTL    , KC_LGUI , KC_LALT   , MO(_FN)   , KC_SPC    , KC_SPC  , KC_LEFT   , KC_DOWN , KC_RGHT , KC_SPC  , KC_SPC  , MO(_FN) , KC_RALT , KC_RGUI , KC_RCTL
 	)
 	,  
 	[_NUMPAD] = LAYOUT_ortho_5x15(   
-		XXXXXXX    , XXXXXXX , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX , XXXXXXX , KC_SLSH , KC_ASTR , KC_MINS , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , _______     , 
-		_______    , XXXXXXX , KC_UP     , XXXXXXX   , XXXXXXX   , XXXXXXX , KC_P7   , KC_P8   , KC_P9   , KC_PLUS , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX     , 
-		TO(_QWERTY), KC_LEFT , KC_DOWN   , KC_RGHT   , XXXXXXX   , XXXXXXX , KC_P4   , KC_P5   , KC_P6   , KC_PLUS , XXXXXXX , XXXXXXX , XXXXXXX , _______ , _______     , 
-		_______    , XXXXXXX , LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_COMM , KC_P1   , KC_P2   , KC_P3   , KC_PENT , XXXXXXX , _______ , _______ , _______ , _______     , 
-		_______    , _______ , _______   , _______   , _______   , _______ , KC_P0   , KC_P0   , KC_PDOT , KC_PENT , XXXXXXX , _______ , _______ , _______ , _______
+		XXXXXXX    , XXXXXXX , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX , XXXXXXX   , KC_SLSH , KC_ASTR , KC_MINS , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , _______     , 
+		_______    , XXXXXXX , KC_UP     , XXXXXXX   , XXXXXXX   , XXXXXXX , KC_P7     , KC_P8   , KC_P9   , KC_PLUS , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX     , 
+		TO(_QWERTY), KC_LEFT , KC_DOWN   , KC_RGHT   , XXXXXXX   , XXXXXXX , KC_P4     , KC_P5   , KC_P6   , KC_PLUS , XXXXXXX , XXXXXXX , XXXXXXX , _______ , _______     , 
+		_______    , XXXXXXX , LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_COMM , KC_P1     , KC_P2   , KC_P3   , KC_PENT , XXXXXXX , _______ , _______ , _______ , _______     , 
+		_______    , _______ , _______   , _______   , _______   , _______ , KC_P0     , KC_P0   , KC_PDOT , KC_PENT , XXXXXXX , _______ , _______ , _______ , _______
 	)		
 	,
 	[_FN] = LAYOUT_ortho_5x15(
-		KC_SLEP , KC_F1   , KC_F2   , KC_F3   , KC_F4    , KC_F5   , RGB_HUI   , RGB_VAI , RGB_SAI , KC_F6   , KC_F7   , KC_F8   , KC_F9     , KC_F10    , XXXXXXX    , 
-		XXXXXXX , KC_F11  , KC_F12  , XXXXXXX , XXXXXXX  , XXXXXXX , RGB_HUD   , RGB_VAD , RGB_SAD , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX   , XXXXXXX   , XXXXXXX    , 
-		XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX  , XXXXXXX , RGB_MOD   , KC_INS  , RGB_RMOD, XXXXXXX , XXXXXXX , RESET   , XXXXXXX   , XXXXXXX   , KC_WAKE    ,
-		_______ , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX  , XXXXXXX , KEYLED_TOG, KC_CALC , RGB_TOG , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX   , XXXXXXX   , _______    , 
-		_______ , XXXXXXX , _______ , XXXXXXX , XXXXXXX  , XXXXXXX , KC_VOLD   , KC_MUTE , KC_VOLU , XXXXXXX , XXXXXXX , _______ , XXXXXXX   , XXXXXXX   , LAYOUT    
+		KC_SLEP    , KC_F1   , KC_F2     , KC_F3     , KC_F4     , KC_F5   , RGB_HUI   , RGB_VAI , RGB_SAI , XXXXXXX , XXXXXXX , XXXXXXX , KC_MINS , KC_EQL  , XXXXXXX    ,
+		XXXXXXX    , KC_F6   , KC_F7     , KC_F8     , KC_F9     , KC_F10  , RGB_HUD   , RGB_VAD , RGB_SAD , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX    , 
+		XXXXXXX    , KC_F11  , KC_F12    , XXXXXXX   , XXXXXXX   , XXXXXXX , RGB_MOD   , KC_INS  , RGB_RMOD, XXXXXXX , XXXXXXX , RESET   , XXXXXXX , XXXXXXX , KC_WAKE    ,
+		_______    , XXXXXXX , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX , KEYLED_TOG, KC_CALC , RGB_TOG , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , _______    , 
+		_______    , XXXXXXX , _______   , XXXXXXX   , XXXXXXX   , XXXXXXX , KC_VOLD   , KC_MUTE , KC_VOLU , XXXXXXX , XXXXXXX , _______ , XXXXXXX , XXXXXXX , LAYOUT    
 	)
 };
 
